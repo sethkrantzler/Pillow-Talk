94b764620362c5057785ee3e900175bb27a538ad
Seth Krantzler <sethkrantzler@gmail.com>
2017-02-14 17:17:50 -0600

    Fixed Index and SubIndex access

--- homeCtrl.js
+++ homeCtrl.js
@@ -5,11 +5,11 @@ app.controller('homeCtrl', function ($scope) {
   $scope.statisticsArr = [];
   $scope.graphShow = true;
   $scope.index = 0;
-  $scope.subindex = 0;
-  $scope.dailygraphs = ['https://www.sleepcycle.com/wp-content/uploads/2016/06/sleepcycle_regular_sleep.png', 'http://i.imgur.com/lxAaB6c.jpg','http://i.imgur.com/duQ1FOl.jpg'];
-  $scope.weeklygraphs = ['http://i.imgur.com/qbMBkXL.jpg', 'http://i.imgur.com/vdNAGns.jpg', 'http://i.imgur.com/xpmJRIF.jpg'];
-  $scope.monthlygraphs = ['http://i.imgur.com/BjRG9nl.png', 'http://i.imgur.com/MgBOuoC.jpg', 'http://i.imgur.com/sjMg4Zd.jpg'];
-  $scope.grapharray = [$scope.dailygraphs, $scope.weeklygraphs, $scope.monthlygraphs];
+  $scope.subIndex = 0;
+  $scope.dailyGraphs = ['https://www.sleepcycle.com/wp-content/uploads/2016/06/sleepcycle_regular_sleep.png', 'http://i.imgur.com/lxAaB6c.jpg','http://i.imgur.com/duQ1FOl.jpg'];
+  $scope.weeklyGraphs = ['http://i.imgur.com/qbMBkXL.jpg', 'http://i.imgur.com/vdNAGns.jpg', 'http://i.imgur.com/xpmJRIF.jpg'];
+  $scope.monthlyGraphs = ['http://i.imgur.com/BjRG9nl.png', 'http://i.imgur.com/MgBOuoC.jpg', 'http://i.imgur.com/sjMg4Zd.jpg'];
+  $scope.graphArray = [$scope.dailyGraphs, $scope.weeklyGraphs, $scope.monthlyGraphs];
 
 
   $scope.dailyStats = {
@@ -51,6 +51,14 @@ app.controller('homeCtrl', function ($scope) {
   $scope.statisticsArr.push($scope.dailyStats);
   $scope.statisticsArr.push($scope.weeklyStats);
   $scope.statisticsArr.push($scope.monthlyStats);
+
+  $scope.changeIndex = function(num){
+    $scope.index = num;
+  }
+
+  $scope.changeSubIndex = function(num){
+    $scope.subIndex = num;
+  }
 
 
 });
