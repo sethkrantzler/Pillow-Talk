7d581aac667095211ded9be53944eddaefcd7dd6
Rashad Laher <rashadlaher@gmail.com>
2017-02-14 16:38:31 -0600

    added buttons that update the graph

--- homeCtrl.js
+++ homeCtrl.js
@@ -5,6 +5,13 @@ app.controller('homeCtrl', function ($scope) {
   $scope.statisticsArr = [];
   $scope.graphShow = true;
   $scope.index = 0;
+  $scope.subindex = 0;
+  $scope.dailygraphs = ['https://www.sleepcycle.com/wp-content/uploads/2016/06/sleepcycle_regular_sleep.png', 'http://i.imgur.com/lxAaB6c.jpg','http://i.imgur.com/duQ1FOl.jpg'];
+  $scope.weeklygraphs = ['http://i.imgur.com/qbMBkXL.jpg', 'http://i.imgur.com/vdNAGns.jpg', 'http://i.imgur.com/xpmJRIF.jpg'];
+  $scope.monthlygraphs = ['http://i.imgur.com/BjRG9nl.png', 'http://i.imgur.com/MgBOuoC.jpg', 'http://i.imgur.com/sjMg4Zd.jpg'];
+  $scope.grapharray = [$scope.dailygraphs, $scope.weeklygraphs, $scope.monthlygraphs];
+
+
   $scope.dailyStats = {
     hours: "Total Hours: 8",
     bedtime: "Bedtime: 11:30 PM",
